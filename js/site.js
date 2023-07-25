cda3c4f8fc2763463be67c896e243d3dadfcfe24
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-07-25 15:01:12 -0400

    Added three function and explanation the code

--- js/site.js
+++ js/site.js
@@ -1,13 +1,25 @@
-function displayMessage() {
-  let inputBox = document.getElementById("message");
+function getValues() {
+  let userString = document.getElementById("userString").value;
 
-  let message = inputBox.value;
-
-  Swal.fire({
-    backdrop: false,
-    title: "App Name",
-    text: message,
-  });
+  let revString = reverseString(userString);
+  displayString(revString);
 }
 
+function reverseString(input) {
+  let revString = "";
 
+  for (let i = input.length - 1; i >= 0; i--) {
+    let letter = input[i];
+
+    revString = revString + letter;
+  }
+
+  return revString;
+}
+
+function displayString(output) {
+  document.getElementById("results").textContent = output;
+  let alertBox = document.getElementById("alert");
+
+  alertBox.classList.remove("invisible");
+}
